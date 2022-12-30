966f35cacbc6f561411a706e2a0a6aa5a0d92fbd
Nishit <nishitsuthar123@gmail.com>
2022-12-30 19:40:39 +0530

    30/12 Evening

--- force-app/main/default/aura/CreateTransaction/CreateTransactionController.js
+++ force-app/main/default/aura/CreateTransaction/CreateTransactionController.js
@@ -1,5 +1,41 @@
 ({
-    myAction : function(component, event, helper) {
+    init : function(cmp, event, helper) {
+        cmp.set('v.columns', [
+            {label: 'Material name', fieldName: 'Name', type: 'text', editable: true, typeAttributes: { required: true }},
+            {label: 'Quantity', fieldName: 'Quantity', type: 'number', editable: true, typeAttributes: { required: true } },
+        ]);
+        helper.getProductList(cmp, event, helper);
+        helper.getBatchList(cmp, event, helper);
+    },
 
-    }
+    handleSaveEdition: function (cmp, event, helper) {
+        var Id = cmp.find("batchPicklist").get('v.value');
+        if (Id) {
+            var draftValues = event.getParam('draftValues');
+            helper.saveEdition(cmp, draftValues);
+        }else{
+            helper.showToast('Error', 'Please Select Batch', 'error')
+        }
+    },
+
+    getMaterialData: function(cmp){
+        var Id = cmp.find("industryPicklist").get('v.value');
+        console.log('Id ',Id);
+        var action = cmp.get("c.materialList");
+        action.setParams({ 
+            recordId : Id
+         });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var result = response.getReturnValue();
+                //* for resetting the value to zero
+                result.forEach(data => {
+                    data.Quantity = 0;
+                });
+                cmp.set("v.data", result);
+            }
+        });
+        $A.enqueueAction(action);
+    },
 })
--- force-app/main/default/aura/CreateTransaction/CreateTransactionHelper.js
+++ force-app/main/default/aura/CreateTransaction/CreateTransactionHelper.js
@@ -1,5 +1,41 @@
 ({
-    helperMethod : function() {
-
+    saveEdition : function(cmp, newData) {
+        console.log('get data --> ',{newData});
+        var oldData = cmp.get('v.data');
+        console.log({oldData});
+    },
+    getProductList : function(cmp, event, helper){
+        var action = cmp.get("c.productList");
+        action.setParams({  });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var result = response.getReturnValue();
+                cmp.set("v.Products", result);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    getBatchList : function(cmp, event, helper){
+        var action = cmp.get("c.batchList");
+        action.setParams({  });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var result = response.getReturnValue();
+                cmp.set("v.Batch", result);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    showToast : function(title, message, type) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": title,
+            "message": message,
+            "type": type,
+            "duration": 3000
+        });
+        toastEvent.fire();
     }
 })
